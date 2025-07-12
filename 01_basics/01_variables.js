b8a393230f6d16fb00613f2531576150eb53e352
Subhodeep-Ghosh-21 <156516147+Subhodeep-Ghosh-21@users.noreply.github.com>
2025-07-12 05:43:17 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144552 
+let accountEmail = "subho@google.com"
+var accountPassword = "1234"
+accountCity = "Janai"
+let accountState
+
+// we also store value into a variable without declear its type
+
+// accountId = 2 
+// because accountId is const we cant change its value
+
+// var has no scope , don't use it
+// let is new also it has scope , use it
+
+accountEmail = "ghosh@gmail.com"
+accountPassword = "4567"
+accountCity = "hooghly"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,37 @@
+"Use Strict"; // Treat all js code as newer version
+
+// alert("hello");
+// alert(3 + 3);    //we are using nodejs , so we cant do it here , in browser we can
+
+console.log(3+3);
+console.log("subho");
+
+let name = "subho"
+let age = 21
+let isloggesIn = true
+let state = null
+
+// premetive data types ->
+
+// number +> 2 to power 53
+// bigint   -->  if number is too large
+// string use can '' or ""
+// Boolean --> true or false
+// null --> stand alone value
+// undefined --> its not defined
+// symbol --> use for uniqueness
+
+
+// object
+
+console.log(typeof "subho");
+
+// null is a object in js
+console.log(typeof null);   
+
+// undefined is a undefined type in js
+console.log(typeof undefined);
+
+
+
+
